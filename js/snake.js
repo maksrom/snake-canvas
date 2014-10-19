c173b41838b5d12c91096775ec64987d8677dc03
maksr51314 <maksr51314@gmail.com>
2014-10-19 10:41:38 +0300

    - get canvas context

--- js/snake.js
+++ js/snake.js
@@ -3,7 +3,14 @@
 function onLoad() {
 
     //get canvas element
-    var canvas = document.getElementById( 'snakeId' )
+    var canvas = document.getElementById( 'snakeId' );
+
+    //get 2d context
+    var ctx = canvas.getContext( '2d' );
+
+    //get size for canvas
+    var h = canvas.height;
+    var w = canvas.width;
 
 
 }
